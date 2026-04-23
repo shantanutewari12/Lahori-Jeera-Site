57df2d30b5a836664b0faaeb9bae53e0a9130099
shantanutewari12 <shantanitiwari12@gmail.com>
2026-04-23 22:31:32 +0530

    fix api server build

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,8 +1,9 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
+
+const pinoHttp = require("pino-http");
 
 const app: Express = express();
 
